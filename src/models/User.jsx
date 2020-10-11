2d9c4931658295f33b84939ddd6808ceb1afa24c
Ari Davis <aridavis6688@gmail.com>
2020-10-11 17:49:10 +0700

    Logout

--- src/models/User.jsx
+++ src/models/User.jsx
@@ -4,7 +4,7 @@ class User {
   static Login = (body) => ApiClient.Post("/auth/login", body);
   static Register = (body) => ApiClient.Post("/users/register", body);
 
-  static Logout = () => ApiClient.Post("/logout");
+  static Logout = () => ApiClient.Post("/auth/logout");
 
   static Wallet = () => ApiClient.Get("/wallet");
 }
