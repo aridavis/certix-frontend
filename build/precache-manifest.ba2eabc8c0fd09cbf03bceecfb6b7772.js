ce817cdd081656f77af239d2fc20dde0cac94119
Ari Davis <aridavis6688@gmail.com>
2020-10-12 06:27:31 +0700

    Build

---
+++ build/precache-manifest.ba2eabc8c0fd09cbf03bceecfb6b7772.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae6ba769672c5d78ac4196588492509",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ae1f702442ae80d20ef",
+    "url": "/static/css/main.336003a7.chunk.css"
+  },
+  {
+    "revision": "d414d77e030bf504923f",
+    "url": "/static/js/2.5bccda32.chunk.js"
+  },
+  {
+    "revision": "d4b45ffc0d658f8e0c52a5e0f01e5334",
+    "url": "/static/js/2.5bccda32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ae1f702442ae80d20ef",
+    "url": "/static/js/main.d821812c.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "082bb0f27d6e80d02e1d0cd617d3570d",
+    "url": "/static/media/Gilmer Bold.082bb0f2.ttf"
+  },
+  {
+    "revision": "936fe72bbbfaead885e44b3b2e8b5abd",
+    "url": "/static/media/Gilmer Heavy.936fe72b.ttf"
+  },
+  {
+    "revision": "03189f48089c842f9f71069e8b10de40",
+    "url": "/static/media/Gilmer Light.03189f48.ttf"
+  },
+  {
+    "revision": "3090b8fc0bcd77b4866d2f43d3fa15ec",
+    "url": "/static/media/Gilmer Medium.3090b8fc.ttf"
+  },
+  {
+    "revision": "50771aee7303628888bb8c2badb90020",
+    "url": "/static/media/Gilmer Outline.50771aee.ttf"
+  },
+  {
+    "revision": "2b3bfafecf907b87a0a16789ca710f38",
+    "url": "/static/media/Gilmer Regular.2b3bfafe.ttf"
+  },
+  {
+    "revision": "c1ebdff757737b676868ee30b5efd3f6",
+    "url": "/static/media/concert.c1ebdff7.jpg"
+  },
+  {
+    "revision": "0050b78d1da54d6dbbb516d353631d8d",
+    "url": "/static/media/concert2.0050b78d.jpg"
+  },
+  {
+    "revision": "2f0320d4c354b136152bdb7fcff46ce5",
+    "url": "/static/media/concert3.2f0320d4.jpg"
+  },
+  {
+    "revision": "232084c5bf51ffc095f50187a23fa9bd",
+    "url": "/static/media/equinox.232084c5.ttf"
+  },
+  {
+    "revision": "4a91cccb6839b764a7873648edc4f6c2",
+    "url": "/static/media/hello-beauty.4a91cccb.ttf"
+  },
+  {
+    "revision": "e818a9e1836b80c368a33c9a29f295ad",
+    "url": "/static/media/search.e818a9e1.jpg"
+  },
+  {
+    "revision": "3001a2d286349fff3fa5956ce5fcc312",
+    "url": "/static/media/singer.3001a2d2.jpg"
+  }
+]);
